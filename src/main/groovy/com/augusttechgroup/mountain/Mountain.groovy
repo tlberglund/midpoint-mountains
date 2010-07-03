2105aa2768a8f50e205f4a6a49590a07d0fb609f
tlberglund <tlberglund@gmail.com>
2010-07-03 10:55:28 -0600

    Removed known-obsolete Mountain code.

--- src/main/groovy/com/augusttechgroup/mountain/Mountain.groovy
+++ src/main/groovy/com/augusttechgroup/mountain/Mountain.groovy
@@ -136,33 +136,6 @@ m.displaceRow(oldRow)
   }
 
 
-  def buildInitialRange(displacer = null, scale = 1) {
-    def northWest = new Point(displacer: displacer, scale: scale)
-    def northEast = new Point(displacer: displacer, scale: scale)
-    def southEast = new Point(displacer: displacer, scale: scale)
-    def southWest = new Point(displacer: displacer, scale: scale)
-    
-    northWest.east = northEast
-    northWest.south = southWest
-    
-    northEast.west = northWest
-    northEast.south = southEast
-    
-    southEast.north = northEast
-    southEast.west = southWest
-    
-    southWest.north = northWest
-    southWest.east = southEast
-    
-//    northWest.displace()
-//    northEast.displace()
-//    southEast.displace()
-//    southWest.displace()
-    
-    return [[northWest, northEast], [southWest, southEast]]
-  }
-
-
   def grow() {
     scale++
 
@@ -245,57 +218,6 @@ m.displaceRow(oldRow)
     return rows
   }
 
-
-  /**
-   * Converts the elevation graph to a two-dimensional list of elevation values.
-   */
-  def toArray() {
-    northWest.collectSouth { nsPoint ->
-      nsPoint.collectEast { ewPoint -> ewPoint.elevation }
-    }.toArray()
-  }
-
-
-  def doInsertionsOnEastWestRow(westernPoint, scale, displacer = null) {
-    westernPoint.eachEast { point ->
-      if(point.east) {
-        def newPoint = point.insertEast(scale, displacer)
-        newPoint.displace((point.elevation + newPoint.east.elevation) / 2)
-      }
-    }
-  }
-
-
-  def insertBetweenRows(northRow, southRow, scale, displacer = null) {
-    def northIterator = northRow.eastIterator()
-    def southIterator = southRow.eastIterator()
-    def lastMiddlePoint
-    def firstMiddlePoint
-    
-    while(northIterator.hasNext()) {
-      def northPoint = northIterator?.next()
-      def southPoint = southIterator?.next()
-      
-      def middlePoint = new Point()
-      middlePoint.scale = scale
-      middlePoint.displacer = displacer
-      middlePoint.north = northPoint
-      middlePoint.south = southPoint
-      middlePoint.west = lastMiddlePoint
-      if(lastMiddlePoint) lastMiddlePoint.east = middlePoint
-      
-      northPoint.south = middlePoint
-      southPoint.north = middlePoint
-      
-      middlePoint.displace((northPoint.elevation + southPoint.elevation) / 2)
-      
-      lastMiddlePoint = middlePoint
-      if(!firstMiddlePoint) firstMiddlePoint = middlePoint
-    }
-    
-    return firstMiddlePoint
-  }
-  
   
   String toString() {
     rows.toString()
